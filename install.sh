ab5ef772ad602bfa19b7fc3217aa9d3296c13e8a
Bruce E. Thelen <926473+bthelen@users.noreply.github.com>
2021-05-13 14:15:02 -0500

    More vim automation

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 
 set -e
 
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 cp vimrc ~/.vimrc
+vim +PluginInstall +qall
 
 cp starship.toml ~/.config/starship.toml
 
