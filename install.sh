455a35990a834277af4670a8c8e57978cb52c7f2
Bruce E. Thelen <926473+bthelen@users.noreply.github.com>
2026-09-07 16:32:48 -0500

    tab to space

--- install.sh
+++ install.sh
@@ -4,15 +4,15 @@ set -e
 
 # get my preferred font
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	# ...
-	echo "will install this font later"
+  # ...
+  echo "will install this font later"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	# Mac OSX
-	brew install font-jetbrains-mono-nerd-font
+  # Mac OSX
+  brew install font-jetbrains-mono-nerd-font
 fi
 
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
-	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 cp vimrc ~/.vimrc
 vim +PluginInstall +qall
@@ -26,7 +26,7 @@ cp tmux.conf.local ~/.tmux.conf.local
 cp starship.toml ~/.config/starship.toml
 
 mkdir -p ~/.config/yamllint/
-cp yamllint-config  ~/.config/yamllint/config
+cp yamllint-config ~/.config/yamllint/config
 
 mkdir -p ~/.config/ghostty/
 cp config.ghostty ~/.config/ghostty/config.ghostty
